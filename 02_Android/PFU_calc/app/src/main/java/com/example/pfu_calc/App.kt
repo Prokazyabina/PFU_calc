88d5f44410bc422c460582e6afc8d6838e91a0b1
Prokazyabina <prokazyabina@gmail.com>
2025-04-06 12:07:47 +0500

    Графические кнопки, не работает иконка и статусбар

---
+++ 02_Android/PFU_calc/app/src/main/java/com/example/pfu_calc/App.kt
@@ -0,0 +1,10 @@
+package com.example.pfu_calc
+
+import android.app.Application
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Инициализация библиотек и т.д.
+    }
+}
